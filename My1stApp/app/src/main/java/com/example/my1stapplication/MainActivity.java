733e4509cf1d454de113bf7b1458e98c5f72d97d
Liu Yuancheng <liu_yuan_cheng@hotmail.com>
2020-10-19 21:52:53 +0800

    Fixed the bug which caused by R.id.textView2.

--- My1stApp/app/src/main/java/com/example/my1stapplication/MainActivity.java
+++ My1stApp/app/src/main/java/com/example/my1stapplication/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.my1stapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public  static  final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +15,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    
+    public void sendMessage(View view){
+        Intent intent = new Intent(this,DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
 }

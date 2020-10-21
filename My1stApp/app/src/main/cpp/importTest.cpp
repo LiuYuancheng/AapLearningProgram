b209e4c64abe0bacf46a442b1db15f32e7d95d42
Liu Yuancheng <liu_yuan_cheng@hotmail.com>
2020-10-21 22:01:09 +0800

    Added the C compile part.

---
+++ My1stApp/app/src/main/cpp/importTest.cpp
@@ -0,0 +1,18 @@
+//
+// Created by liu_y on 21/10/2020.
+//
+#include <iostream>
+#include <android/log.h>
+
+int functionC()
+{
+    std::cout << "Welcome to leaning C++ for 24 hours ^_^ \n";
+    int choice = 0;
+    //do
+    //{
+     //   std::cout << "Enter the section number(2-23) you want to check the test code, 0 for end , 1 for help: ";
+    //    std::cin >> choice;
+    //} while (choice != 0);
+    std::cout << "\n Program finished ";
+    return 0;
+}

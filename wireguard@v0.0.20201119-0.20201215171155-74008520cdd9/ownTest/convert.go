938beb89a2eee9262c1fe34173b5b52c9938535b
Liu Yuancheng <liu_yuan_cheng@hotmail.com>
2021-06-14 18:33:47 +0800

    Added the UDP test

--- wireguard@v0.0.20201119-0.20201215171155-74008520cdd9/ownTest/convert.go
+++ wireguard@v0.0.20201119-0.20201215171155-74008520cdd9/ownTest/convert.go
@@ -3,10 +3,12 @@ package main
 // This program is used to test how to convert Yi's shared secret to the
 
 import (
+	"bufio"
 	"encoding/hex"
 	"fmt"
+	"log"
+	"net"
 )
-
 
 func ExampleDump() {
 	content := []byte("Go is an open source programming language.")
@@ -26,12 +28,12 @@ type handshake struct {
 	hash [32]byte
 }
 
-func ExampleEncodeToString() {
+func HexStringConvert() {
+	// test convert byte arrary to string and convert back then du the xor
+	//
 	src := []byte("Go is an open source programming language.")
-
 	orgStr := hex.EncodeToString(src)
 	fmt.Printf("%s\n", orgStr)
-
 	var a [32]byte
 	copy(a[:], src)
 
@@ -39,14 +41,47 @@ func ExampleEncodeToString() {
 
 	encodedStr := hex.EncodeToString(s.hash[:])
 
-	fmt.Printf("%s\n", encodedStr)
+	fmt.Printf("encode: %s\n", encodedStr)
 
+	// decode from string
+	decoded, err := hex.DecodeString(encodedStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("decode: %s\n", decoded)
+
+	fmt.Printf("After the xor loop: ")
+	for i := 0; i < len(s.hash); i++ {
+		s.hash[i] ^= src[i]
+	}
+	xorStr := hex.EncodeToString(s.hash[:])
+	fmt.Printf("%s\n", xorStr)
 	// Output:
 	// 48656c6c6f
+
 }
 
+func UDPClient(){
+	p :=  make([]byte, 2048)
+	conn, err := net.Dial("udp", "127.0.0.1:1234")
+	if err != nil {
+		fmt.Printf("Some error %v", err)
+		return
+	}
+	fmt.Fprintf(conn, "Hi UDP Server, How are you doing?")
+	_, err = bufio.NewReader(conn).Read(p)
+	if err == nil {
+		fmt.Printf("%s\n", p)
+	} else {
+		fmt.Printf("Some error %v\n", err)
+	}
+	conn.Close()
+
+}
 
 func main() {
-	ExampleEncodeToString()
+	UDPClient()
+	HexStringConvert()
 	ExampleDump()
 }
